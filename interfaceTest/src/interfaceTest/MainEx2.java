b563d28c753935221e1e49a09ffd9508969d1c30
horokkk <smurfystrawberry@gmail.com>
2024-10-17 23:44:11 +0900

    Create MainEx2.java

---
+++ interfaceTest/src/interfaceTest/MainEx2.java
@@ -0,0 +1,11 @@
+package interfaceTest;
+
+public class MainEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Paint ahn = new Paint();
+		ahn.run();
+	}
+
+}
