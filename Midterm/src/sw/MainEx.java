1c0bbe1208b4456fb644b8cf3f46aee55d880381
horokkk <smurfystrawberry@gmail.com>
2024-10-23 19:52:12 +0900

    Create MainEx.java

---
+++ Midterm/src/sw/MainEx.java
@@ -0,0 +1,8 @@
+package sw;
+
+public class MainEx {
+    public static void main(String[] args) {
+        CoffeeVendingMachine ewha = new CoffeeVendingMachine("이화 커피 자판기");
+        ewha.run();
+    }
+}
